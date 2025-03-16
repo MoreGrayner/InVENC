8ecaad465301e99892ea49ddae235eb17c58ae3d
MoreGrayner <peargrayinyt@gmail.com>
2025-03-16 13:28:31 +0900

    Initial commit

---
+++ src/main/kotlin/io/github/moregrayner/inVENC1/InVENC1.kt
@@ -0,0 +1,14 @@
+package io.github.moregrayner.inVENC1
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class InVENC1 : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
